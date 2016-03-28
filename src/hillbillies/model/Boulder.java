2815b86b7d01ca3b23ff3464de7ea6ba157965ba
Ellen <anthonissen.ellen@gmail.com>
2016-03-28 22:26:05 +0200

    boulder en log

--- src/hillbillies/model/Boulder.java
+++ src/hillbillies/model/Boulder.java
@@ -2,4 +2,8 @@ package hillbillies.model;
 
 public class Boulder extends Material {
 
+	public Boulder(World world, Position position) throws IllegalArgumentException {
+		super(world, position);
+	}
+
 }
