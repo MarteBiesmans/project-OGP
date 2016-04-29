4ec077ae8f9f8de0d0e67a75d88c7d66c3994a93
MarteBiesmans <n-abbe@hotmail.com>
2016-04-29 11:05:56 +0200

    te veel om te committen, dus zie vorige commit

--- src/hillbillies/model/programs/expressions/LogExpression.java
+++ src/hillbillies/model/programs/expressions/LogExpression.java
@@ -4,26 +4,30 @@ import hillbillies.model.Cube;
 import hillbillies.model.Log;
 import hillbillies.model.Unit;
 
-public class LogExpression extends CubePositionExpression {
+public class LogExpression extends NullaryCubeExpression {
 
-	public LogExpression(Unit unit) {
-		super(unit);
+	public LogExpression() {
+		super();
 	}
 
 	@Override
-	public Cube evaluate() {
-		Unit unit = (Unit) getObject();
+	public Cube evaluate(Unit unit) {
+		if (unit.getWorld().getAllLogs().size() == 0)
+			return unit.getCube();
 		Log nearestLogSoFar = null;
-		for (Log log: unit.getWorld().getAllLogs()) {
+		for (Log log : unit.getWorld().getAllLogs()) {
 			if (nearestLogSoFar == null)
 				nearestLogSoFar = log;
-			else if (unit.getPosition().getDistanceSquare(log.getPosition()) < unit.getPosition().getDistanceSquare(nearestLogSoFar.getPosition()))
-					nearestLogSoFar = log;
+			else if (unit.getPosition().getDistanceSquare(log.getPosition()) < unit.getPosition()
+					.getDistanceSquare(nearestLogSoFar.getPosition()))
+				nearestLogSoFar = log;
 		}
-		if (nearestLogSoFar != null)
-			return nearestLogSoFar.getPosition().getCube();
-		else
-			return unit.getCube();
+		return nearestLogSoFar.getPosition().getCube();
+	}
+
+	@Override
+	public String toString() {
+		return "log";
 	}
 
 }
--- src/hillbillies/model/programs/expressions/WorkshopExpression.java
+++ src/hillbillies/model/programs/expressions/WorkshopExpression.java
@@ -1,29 +1,33 @@
 package hillbillies.model.programs.expressions;
 
-import hillbillies.model.Boulder;
 import hillbillies.model.Cube;
 import hillbillies.model.Unit;
 
-public class WorkshopExpression extends CubePositionExpression {
+public class WorkshopExpression extends NullaryCubeExpression {
 
-	public WorkshopExpression(Unit unit) {
-		super(unit);
+	public WorkshopExpression() {
+		super();
 	}
 
 	@Override
-	public Cube evaluate() {
-		Unit unit = (Unit) getObject();
+	public Cube evaluate(Unit unit) {
 		Cube nearestWorkshopSoFar = null;
-		for (Cube workshop: unit.getWorld().getAllWorkshops()) {
+		for (Cube workshop : unit.getWorld().getAllWorkshops()) {
 			if (nearestWorkshopSoFar == null)
 				nearestWorkshopSoFar = workshop;
-			else if (unit.getPosition().getDistanceSquare(workshop.getCenter()) < unit.getPosition().getDistanceSquare(nearestWorkshopSoFar.getCenter())))
-					nearestWorkshopSoFar = new Cube(workshop.getX(), workshop.getY(), workshop.getZ());
+			else if (unit.getPosition().getDistanceSquare(workshop.getCenter()) < unit.getPosition()
+					.getDistanceSquare(nearestWorkshopSoFar.getCenter()))
+				nearestWorkshopSoFar = new Cube(workshop.getX(), workshop.getY(), workshop.getZ());
 		}
 		if (nearestWorkshopSoFar != null)
 			return nearestWorkshopSoFar;
 		else
 			return unit.getCube();
+	}
+
+	@Override
+	public String toString() {
+		return "";
 	}
 
 }
