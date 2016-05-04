f6c2b34e498130f35693cd2473880809669d4ef9
MarteBiesmans <n-abbe@hotmail.com>
2016-05-04 11:38:19 +0200

    SelectedExpression oké

--- src/hillbillies/model/programs/expressions/SelectedExpression.java
+++ src/hillbillies/model/programs/expressions/SelectedExpression.java
@@ -1,21 +1,33 @@
 package hillbillies.model.programs.expressions;
 
+import hillbillies.model.Cube;
 import hillbillies.model.Unit;
 import hillbillies.model.programs.type.CubeType;
 
 public class SelectedExpression extends CubeExpression {
 
-	public SelectedExpression() {
+	public SelectedExpression(XYZExpression e) {
+		expression = e;
 	}
+	
+	public XYZExpression getExpression() {
+		return expression;
+	}
+	
+	public CubeType getExpressionEvaluate(Unit unit) {
+		return new CubeType((Cube) getExpression().evaluate(unit).getValue());
+	}
+	
+	private XYZExpression expression;
 	
 	@Override
 	public CubeType evaluate(Unit unit) {
-		return new CubeType(null);
+		return new CubeType((Cube) getExpressionEvaluate(unit).getValue());
 	}
 	
 	@Override
 	public String toString(){
-		return "";
+		return "selected";
 	}
 
 }
