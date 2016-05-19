45b9a7e47efd5c9f98b252af85dcaa14ae40086b
MarteBiesmans <n-abbe@hotmail.com>
2016-05-19 15:28:43 +0200

    distpair gefixt

--- src/hillbillies/model/DistPair.java
+++ src/hillbillies/model/DistPair.java
@@ -1,9 +1,34 @@
 package hillbillies.model;
 
-public class DistPair {
+@SuppressWarnings("rawtypes")
+public class DistPair<T> implements Comparable<DistPair> {
+	
+	private final Double distance;
+	private final T thing;
 
-	public DistPair() {
-		// TODO Auto-generated constructor stub
+	public DistPair(T thing, Double distance) {
+		this.thing = thing;
+		this.distance = distance;
 	}
 
+	public Double getDistance() {
+		return distance;
+	}
+
+	public T getThing() {
+		return thing;
+	}
+
+	@Override
+	public int compareTo(DistPair other) {
+		return this.getDistance().compareTo(other.getDistance());
+	}
+	
+	public DistPair getMinimum(DistPair other) {
+		if (this.getDistance() <= other.getDistance())
+			return this;
+		else
+			return other;
+	}
+	
 }
